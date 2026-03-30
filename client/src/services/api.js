abb0e9941158484af4f4e9a2527347b2f57f6e49
Y22ACS594 <reddymouli74@gmail.com>
2026-03-30 21:20:03 +0530

    mobile  view

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,9 +1,14 @@
 import axios from "axios";
-
+/*
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000/api",
-});
+  
+});*/
 
+const api = axios.create({
+  baseURL: import.meta.env.VITE_API_URL || "http://192.168.210.9:5000/api",
+  
+});
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
   if (token) {
